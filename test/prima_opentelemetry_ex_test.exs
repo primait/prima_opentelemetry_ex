76243d2a6245ee7024c7b5a487ea7cd8d4a9c8f7
Matteo Busi <mbusi81@gmail.com>
2021-09-29 12:40:47 +0200

    Remove useless test

--- test/prima_opentelemetry_ex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PrimaOpentelemetryExTest do
-  use ExUnit.Case
-  doctest PrimaOpentelemetryEx
-
-  test "greets the world" do
-    assert PrimaOpentelemetryEx.hello() == :world
-  end
-end
