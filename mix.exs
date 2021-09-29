f083c2a469d5fe476dd8f6e4028afb2783d8b2c7
Matteo Busi <mbusi81@gmail.com>
2021-09-29 11:21:49 +0200

    Add opentelemetry_absinthe dependency

--- mix.exs
+++ mix.exs
@@ -11,7 +11,6 @@ defmodule PrimaOpentelemetryEx.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
@@ -21,8 +20,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:opentelemetry_absinthe, git: "git@github.com:primait/opentelemetry_absinthe.git", branch: "wizard"}
     ]
   end
 end
