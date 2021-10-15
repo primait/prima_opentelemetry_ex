d62007daf921a5c0fdb8e3f7d97073539d99f42d
Matteo Busi <mbusi81@gmail.com>
2021-10-15 14:54:22 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   def project do
     [
       app: :prima_opentelemetry_ex,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
