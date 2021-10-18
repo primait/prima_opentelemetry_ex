5f9166048e10077734d5f3b737b2f54167d8641b
Matteo Busi <mbusi81@gmail.com>
2021-10-18 16:10:22 +0200

    Format

--- test/resource_detector_test.exs
+++ test/resource_detector_test.exs
@@ -3,7 +3,10 @@ defmodule PrimaOpentelemetryEx.ResourceDetectorTest do
 
   describe "use default service name and version if env vars aren't set" do
     assert {:otel_resource, resource} = PrimaOpentelemetryEx.ResourceDetector.get_resource([])
-    assert {'service.name', 'prima-opentelemetry-service'} = List.keyfind(resource, 'service.name', 0)
+
+    assert {'service.name', 'prima-opentelemetry-service'} =
+             List.keyfind(resource, 'service.name', 0)
+
     assert {'service.version', '0.0.0-dev'} = List.keyfind(resource, 'service.version', 0)
   end
 
@@ -12,7 +15,7 @@ defmodule PrimaOpentelemetryEx.ResourceDetectorTest do
     assert {:otel_resource, resource} = PrimaOpentelemetryEx.ResourceDetector.get_resource([])
     assert {'service.name', 'test'} = List.keyfind(resource, 'service.name', 0)
   end
-  
+
   describe "use VERSION env var to set service.version" do
     System.put_env("VERSION", "1.2.3-test")
     assert {:otel_resource, resource} = PrimaOpentelemetryEx.ResourceDetector.get_resource([])
