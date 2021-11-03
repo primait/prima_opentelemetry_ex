1d257de97e05f78122518a6906810e375a21e2aa
Matteo Busi <mbusi81@gmail.com>
2021-11-03 17:36:07 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/prima_opentelemetry_ex"
-  @version "1.0.0-rc.2"
+  @version "1.0.0-rc.3"
 
   def project do
     [
