240d9a3e897f4c4447900c753ec3aa988531bc32
Francesco Saltori <francescosaltori@gmail.com>
2022-09-09 15:35:35 +0200

    Prepare 1.1.2 release

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/prima_opentelemetry_ex"
-  @version "1.1.1"
+  @version "1.1.2"
 
   def project do
     [
