443a398b9750a058a23744bcca7a765dcc419fa2
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-09-22 10:40:28 +0200

    add .versions.yml & bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/prima_opentelemetry_ex"
-  @version "1.1.2"
+  @version "1.1.3"
 
   def project do
     [
