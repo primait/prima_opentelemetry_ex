de4f2c06080b92ea175d85a1f4b45aa613a937e6
Cristiano Piemontese <cristiano.piemontese@prima.it>
2023-08-28 16:32:16 +0200

    bump min version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
     [
       app: :prima_opentelemetry_ex,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
