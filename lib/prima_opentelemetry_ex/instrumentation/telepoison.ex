21f915a1a8f5d6a60e936bcfde864cfaeb23a3fc
Simone Cottini <cottini.simone@gmail.com>
2023-09-01 09:55:46 +0200

    [PLATFORM-1254]: Avoid using Telepoison.setup() since it is deprecated (#72)
    * Remove Telepoison.setup call. User should use config
    
    * Remove Telepoison.maybe_setup
    
    * Format + unused

--- lib/prima_opentelemetry_ex/instrumentation/telepoison.ex
+++
@@ -1,9 +0,0 @@
-defmodule PrimaOpentelemetryEx.Instrumentation.Telepoison do
-  @moduledoc false
-
-  if Code.ensure_loaded?(Telepoison) do
-    def maybe_setup, do: Telepoison.setup()
-  else
-    def maybe_setup, do: nil
-  end
-end
