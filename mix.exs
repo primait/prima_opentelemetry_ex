fc531d81b97e7d601e2b04c67c86628622de114e
MaeIsBad <26093674+MaeIsBad@users.noreply.github.com>
2024-05-03 14:14:11 +0200

    Prep release (#108)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/primait/prima_opentelemetry_ex"
-  @version "2.1.0-pre.0"
+  @version "2.1.0"
 
   def project do
     [
