5161363eb98067a8244477b88cb1d8f890ee19d7
Nicola Baraldo <nicola.baraldo@prima.it>
2024-06-17 10:36:17 +0200

    Remove Elixir anonymous function warning on telemetry:attach/4

--- lib/prima_opentelemetry_ex/instrumentation/opentelemetry_ecto.ex
+++ lib/prima_opentelemetry_ex/instrumentation/opentelemetry_ecto.ex
@@ -4,18 +4,20 @@ defmodule PrimaOpentelemetryEx.Instrumentation.OpentelemetryEcto do
   alias PrimaOpentelemetryEx.Instrumentation.Optional
   require Optional
 
+  def repo_init_handler(_event, _measurements, metadata, _config) do
+    metadata
+    |> Map.fetch!(:opts)
+    |> Keyword.fetch!(:telemetry_prefix)
+    |> OpentelemetryEcto.setup()
+  end
+
   Optional.instrument Ecto,
     with: OpentelemetryEcto,
     feature: :ecto do
     :telemetry.attach(
       "repo-init-handler",
       [:ecto, :repo, :init],
-      fn _event, _measurements, metadata, _config ->
-        metadata
-        |> Map.fetch!(:opts)
-        |> Keyword.fetch!(:telemetry_prefix)
-        |> OpentelemetryEcto.setup()
-      end,
+      &__MODULE__.repo_init_handler/4,
       %{}
     )
   end
