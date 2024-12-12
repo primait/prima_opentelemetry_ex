7bd04f6d987a4c3771223028eb763f5da386172d
Jaume Delclòs Coll <jaume.delclos@prima.it>
2024-12-12 11:47:06 +0100

    allow teleplug ~> 2.0

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule PrimaOpentelemetryEx.MixProject do
     [
       {:opentelemetry_absinthe, "~> 2.0", optional: true},
       {:opentelemetry_ecto, "~> 1.0", optional: true},
-      {:teleplug, "~> 1.1", optional: true}
+      {:teleplug, "~> 1.1 or ~> 2.0", optional: true}
     ]
   end
 
